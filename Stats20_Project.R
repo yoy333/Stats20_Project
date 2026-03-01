c278d3382bff1daef4006301837868bcc84178b0
Krish Matai <52080575+KrishM23@users.noreply.github.com>
2026-02-28 21:42:11 -0800

    added the data loading and race/age ggplots

---
+++ Stats20_Project.R
@@ -0,0 +1,56 @@
+# Investigating Healthcare Inequalities in LA County
+
+library(ggplot2)
+library(readxl)
+
+#loading the excel data
+health_data <- read_excel("data/LACHS2023_Adult_DiffAccCare.xlsx", skip = 6, col_names = FALSE)
+
+names(health_data) <- c("col1", "group", "flag", "proportion", "ci_low", "dash", "ci_high", "estimated_n")
+
+# keep only useful columns
+health_data <- health_data[, c("group", "flag", "proportion", "ci_low", "ci_high", "estimated_n")]
+
+head(health_data)
+#noticed a lot of nas ! rip
+str(health_data)
+
+#cleaning up a bit
+health_clean <- health_data[!is.na(health_data$proportion), ]
+health_clean <- health_clean[!is.na(health_data$group), ]
+
+#convering to %
+health_clean$percent <- health_clean$proportion * 100
+health_clean
+
+# one thing we can do is plot % difficulty by race/ethnicity
+race_groups <- c("Latinx", "NH White", "NH Black or African American", 
+                 "NH Asian", "NH Native Hawaiian or Pacific Islander",
+                 "NH American Indian or Alaska Native", "NH Multi-Racial or Other Race")
+
+#check for this in our clean data
+race_data <- health_clean[health_clean$group %in% race_groups, ]
+
+#plotting :D
+ggplot(race_data, aes(x = group, y = percent, fill = group)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Difficulty Accessing Healthcare By Race/Ethnicity", 
+       subtitle = "LA County Health Survey 2023", 
+       x = "Race/Ethnicity", y = "Percent (%)") + 
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+#Latinx most
+
+# we could also plot by age group
+age_groups <- c("18-24", "25-29", "30-39", "40-49", "50-59", "60-64", "65 or over")
+
+age_data <- health_clean[health_clean$group %in% age_groups, ]
+ggplot(age_data, aes(x = group, y = percent, fill = group)) + 
+  geom_bar(stat = "identity") + 
+  labs(title = "Difficulty Accessing Healthcare By Age Group",
+       subtitle = "LA County Health Survey 2023", 
+       x = "Age Group", y = "Percent (%)") + 
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+# 25-29 most
+  
