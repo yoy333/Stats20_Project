f436201832c366111ca71abd8327d494a9df99d5
adidmahm <adidmahmood1234@gmail.com>
2026-03-14 16:32:25 -0700

    polished some of the plots and changed the grouping for the region plots

--- Stats20_Project.R
+++ Stats20_Project.R
@@ -17,7 +17,7 @@ str(health_data)
 
 #cleaning up a bit
 health_clean <- health_data[!is.na(health_data$proportion), ]
-health_clean <- health_clean[!is.na(health_data$group), ]
+health_clean <- health_clean[!is.na(health_clean$group), ]
 
 #convering to %
 health_clean$percent <- health_clean$proportion * 100
@@ -31,14 +31,30 @@ race_groups <- c("Latinx", "NH White", "NH Black or African American",
 #check for this in our clean data
 race_data <- health_clean[health_clean$group %in% race_groups, ]
 
+clean_theme <- theme_minimal(base_size = 13) + 
+  theme(
+    legend.position    = "none",
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor   = element_blank(),
+    axis.title.y       = element_blank(),
+    plot.title         = element_text(face = "bold", size = 14),
+    plot.subtitle      = element_text(color = "gray50", size = 11),
+    plot.caption       = element_text(color = "gray60", size = 9)
+  )
+
 #plotting :D
-ggplot(race_data, aes(x = group, y = percent, fill = group)) +
+ggplot(race_data, aes(x = reorder(group, percent), y = percent, fill = percent)) +
   geom_bar(stat = "identity") +
+  geom_text(aes(label = paste0(round(percent, 1), "%")),
+            hjust = -0.15, size = 3.5, color = "gray20") +
+  scale_fill_gradient(low = "#c6dbef", high = "#08519c") +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.18))) +
+  coord_flip() +
   labs(title = "Difficulty Accessing Healthcare By Race/Ethnicity", 
        subtitle = "LA County Health Survey 2023", 
-       x = "Race/Ethnicity", y = "Percent (%)") + 
-  theme_minimal() + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+       x = "Race/Ethnicity", y = "Percent (%)") +
+  clean_theme
+
 #Latinx most
 
 # we could also plot by age group
@@ -59,9 +75,11 @@ print(health_clean, n = 83)
 poverty_groups <- c("0-99% FPL", "100%-199% FPL", "200%-299% FPL", "300% or above FPL")
 
 poverty_data <- health_clean[health_clean$group %in% poverty_groups, ]
+poverty_data$group <- factor(poverty_data$group, levels = poverty_groups)
 
 ggplot(poverty_data, aes(x = group, y = percent, fill = group))+
-  geom_bar(stat = "identity")+
+  geom_bar(stat = "identity") + 
+  scale_fill_manual(values = colorRampPalette(c("#b2182b","#f4a582","#92c5de","#2166ac"))(length(poverty_groups))) +
   labs(title = "Difficulty Accessing Healthcare by Income Level", 
        subtitle = "LA County Health Survey 2023", 
        x = "Federal Poverty Level", y = "Percent (%)") +
@@ -69,19 +87,75 @@ ggplot(poverty_data, aes(x = group, y = percent, fill = group))+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 # seems like 100% - 199% federal poverty level struggles most, as they're just above the poverty line, so they don't receive as much support as 0-99%
 
-#inequalities by region too, lastly
-region_groups <- c("Antelope Valley", "San Fernando", "San Gabriel", 
-                   "Metro", "West", "South", "East", "South Bay")
 
-region_data <- health_clean[health_clean$group %in% region_groups, ]
-ggplot(region_data, aes(x = group, y = percent, fill = group)) +
-  geom_bar(stat = "identity") + 
-  labs(title = "Difficulty Accessing Healthcare by Region", 
-       subtitle = "LA County Health Survey 2023",
-       x = "Region", y = "Percent (%)") + 
-  theme_minimal() + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+district_groups <- c("Alhambra", "Antelope Valley", "Bellflower", "Central", "Compton", 
+                     "East LA", "East Valley", "El Monte", "Foothill", "Glendale", 
+                     "Harbor", "Hollywood-Wilshire", "Inglewood", "Long Beach", 
+                     "Northeast", "Pasadena", "Pomona", "San Antonio", "San Fernando", 
+                     "South", "Southeast", "Southwest", "Torrance", "West", 
+                     "West Valley", "Whittier")
+district_data <- health_clean[health_clean$group %in% district_groups, ]
+
+# geographic groups of all the districts for a North South East West and Central La split
+district_data$geo_group <- NA
+district_data$geo_group[district_data$group %in% c("Antelope Valley", "San Fernando", "East Valley", "West Valley", "Glendale", "Foothill")] <- "North"
+district_data$geo_group[district_data$group %in% c("Central", "Northeast")]                                                                   <- "Central"
+district_data$geo_group[district_data$group %in% c("East LA", "El Monte", "San Antonio", "Pomona", "Pasadena", "Whittier", "Alhambra")]        <- "East"
+district_data$geo_group[district_data$group %in% c("South", "Southeast", "Southwest", "Inglewood", "Compton", "Harbor", "Long Beach", "Torrance", "Bellflower")] <- "South"
+district_data$geo_group[district_data$group %in% c("West", "Hollywood-Wilshire")]                                                             <- "West"
+
+district_data$group <- factor(district_data$group, levels = c(
+  "Antelope Valley", "San Fernando", "East Valley", "West Valley", "Glendale", "Foothill",  # North
+  "Central", "Northeast",                                                                     # Central
+  "Alhambra", "El Monte", "East LA", "San Antonio", "Pomona", "Pasadena", "Whittier",        # East
+  "South", "Southeast", "Southwest", "Inglewood", "Compton", "Harbor", "Long Beach", "Torrance", "Bellflower", # South
+  "Hollywood-Wilshire", "West"                                                                # West
+))
+geo_colors <- c(
+  # North  blue shades
+  "Antelope Valley" = "#08306b", "San Fernando" = "#2171b5",
+  "East Valley"     = "#4292c6", "West Valley"  = "#6baed6",
+  "Glendale"        = "#9ecae1", "Foothill"     = "#c6dbef",
+  # Central  green shades
+  "Central"  = "#238b45", "Northeast" = "#74c476",
+  # East  orange shades
+  "Alhambra"    = "#7f2704", "El Monte"   = "#a63603",
+  "East LA"     = "#d94801", "San Antonio"= "#f16913",
+  "Pomona"      = "#fd8d3c", "Pasadena"   = "#fdae6b",
+  "Whittier"    = "#fdd0a2",
+  # South - red shades
+  "South"       = "#67001f", "Southeast"  = "#a50026",
+  "Southwest"   = "#d73027", "Inglewood"  = "#f46d43",
+  "Compton"     = "#fdae61", "Harbor"     = "#fee090",
+  "Long Beach"  = "#d7191c", "Torrance"   = "#e85d5d",
+  "Bellflower"  = "#f5a0a0",
+  # West - purple
+  "Hollywood-Wilshire" = "#4d004b", "West" = "#9970ab"
+)
+
+ggplot(district_data, aes(x = group, y = percent, fill = group)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = geo_colors) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.3))) + 
+  facet_wrap(~ geo_group, scales = "free_y", ncol = 2) +
+  coord_flip() +
+  labs(title = "Difficulty Accessing Healthcare by Health District", 
+       subtitle = "% of adults reporting difficulty",
+       x = "Region", y = "Percent (%)") +
+  clean_theme
 #South seems highest
+
+geo_summary <- aggregate(percent ~ geo_group, data = district_data, FUN = mean)
+
+ggplot(geo_summary, aes(x = geo_group, y = percent, fill = geo_group)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = c("North" = "#2171b5", "Central" = "#238b45", 
+                               "East"  = "#d94801", "South"   = "#a50026", 
+                               "West"  = "#4d004b")) +
+  labs(title = "Difficulty Accessing Healthcare by Region",
+       x = "Region", y = "Percent (%)") +
+  theme_minimal() +
+  theme(legend.position = "none")
 
 race_model <- lm(percent ~ group, data = race_data)
 summary(race_model)
